52a34c01027f6443d9f75594490c44ff39944a7b
wmzy <1256573276@qq.com>
2026-05-27 23:07:27 +0800

    fix: move validator registration to useEffect

--- src/hooks/validate.js
+++ src/hooks/validate.js
@@ -13,31 +13,37 @@ import {isPromise} from '../util';
 export default function useValidate(validate, path) {
   const form = useFormContext();
   const lockRef = useRef(null);
+  const validateRef = useRef(validate);
+  validateRef.current = validate;
 
-  form.validators.set(path.key, () => {
-    if (!validate) return;
-    const result = validate(getValueByPath(form, path), {form, path});
-    if (!isPromise(result)) {
-      setErrorByPath(form, path, result);
-      return;
-    }
-    const lock = (lockRef.current = {});
-    setValidatingByPath(form, path);
-    result
-      .then(error => {
-        if (lock === lockRef.current) {
-          setErrorByPath(form, path, error);
-        }
-      })
-      .finally(() => {
-        if (lock === lockRef.current) {
-          unsetValidatingByPath(form, path);
-          lockRef.current = null;
-        }
-      });
-  });
+  useEffect(() => {
+    const validator = () => {
+      const fn = validateRef.current;
+      if (!fn) return;
+      const result = fn(getValueByPath(form, path), {form, path});
+      if (!isPromise(result)) {
+        setErrorByPath(form, path, result);
+        return;
+      }
+      const lock = (lockRef.current = {});
+      setValidatingByPath(form, path);
+      result
+        .then(error => {
+          if (lock === lockRef.current) {
+            setErrorByPath(form, path, error);
+          }
+        })
+        .finally(() => {
+          if (lock === lockRef.current) {
+            unsetValidatingByPath(form, path);
+            lockRef.current = null;
+          }
+        });
+    };
 
-  useEffect(() => () => form.validators.delete(path.key), [form, path.key]);
+    form.validators.set(path.key, validator);
+    return () => form.validators.delete(path.key);
+  }, [form, path.key]);
 
-  return useStageFn(() => form.validators.get(path.key)());
+  return useStageFn(() => form.validators.get(path.key)?.());
 }
